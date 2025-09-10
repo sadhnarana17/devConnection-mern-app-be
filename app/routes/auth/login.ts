ce6f06d42bdff0aaa096f5081e302930c390f722
sadhnarana17 <sadhnarana@founderandlightning.com>
2025-09-10 08:03:05 +0530

    create rest api to login

--- app/routes/auth/login.ts
+++ app/routes/auth/login.ts
@@ -1,9 +1,29 @@
 import { Router } from "express";
+import User from "../../models/user";
 
 const loginRouter = (router: Router) => {
     router.post('/login', async (req, res) => {
-        // Logic to authenticate user
-        res.status(200).send({ message: 'Login successful' });
+        try {
+            const { body: { email, password } } = req;
+            const user = await User.findOne({ emailId: email });
+            if (!user) {
+                return res.status(401).send({ message: 'Invalid email or password' });
+            }
+            const isPasswordValid = await user.checkValidPassword(password);
+            if (!isPasswordValid) {
+                return res.status(401).send({ message: 'Invalid email or password' });
+            }
+            const token = await user.getJWT();
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            });
+
+            res.status(200).send({ message: 'Login successful' });
+        } catch (error) {
+            res.status(500).send({ message: 'Internal server error', error: error.message });
+        }
     });
 }
 
