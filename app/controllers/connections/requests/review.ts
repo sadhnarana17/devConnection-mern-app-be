c552608d4d9beec55388752d3e24ea2439e601de
sadhnarana17 <sadhnarana@founderandlightning.com>
2025-09-11 11:45:19 +0530

    review API endpoint

--- app/controllers/connections/requests/review.ts
+++ app/controllers/connections/requests/review.ts
@@ -1,8 +1,46 @@
-import { Router } from "express";
+import { Router, Request as RequestType, Response } from "express";
+import Request from "../../../models/request";
+import User from "../../../models/user";
+import { authMiddleware } from "../../../middleware/auth";
+
+interface RequestParams extends RequestType {
+    params: {
+        review: 'accepted' | 'rejected';
+        fromUserId: string;
+    };
+    user: InstanceType<typeof User>;
+}
+
+const REVIEW_REQUEST = ['accepted', 'rejected'];
 
 export const reviewRequestsRouter = (router: Router) => {
-    router.post('/connections/requests/:review', async (req, res) => {
-        // Logic to review connection requests
-        res.status(200).send({ message: 'Connection request reviewed successfully' });
+    router.post('/connections/requests/:review/:', authMiddleware, async (req: RequestParams, res: Response) => {
+        try {
+         const toUserId = req.user._id;
+         const {fromUserId, review} = req.params;
+
+         if(!REVIEW_REQUEST.includes(req.params.review)){
+            return res.status(400).send({message: 'Invalid value for :review parameter'});
+         }
+         if(toUserId === fromUserId){
+            return res.status(400).send({message: 'Reviewer cannot accept it/\'s own request'});
+         }
+         const isRequestExistOrReviewed= Request.findOne({
+            $or: [{fromUserId: fromUserId, toUserId: toUserId}, {fromUserId: toUserId, toUserId: fromUserId }],
+            status: {$in: REVIEW_REQUEST}
+         });
+
+        if(isRequestExistOrReviewed){
+            return res.status(400).send({message: 'Request already reviewed or no request found'});
+        }
+        await Request.findOneAndUpdate(
+            {fromUserId: fromUserId, toUserId: toUserId},
+            {status: review},
+            {new: true}
+        );
+         return res.status(200).send({ message: 'Connection request reviewed successfully' });
+        } catch (error) {
+            return res.status(500).send({ message: 'Internal server error', error: error.message });
+        }
     });
 }
